539ec25d3a2648ed793047cb8329c3c909b87a32
hana <hanadegefa344@gmail.com>
2022-12-21 05:01:44 -0800

    mil

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *mul - function that multpliestwo numbers
+ *@: first number
+ *@b: second number
+ *Return: returns result 
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
