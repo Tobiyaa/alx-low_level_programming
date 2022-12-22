a6b6b8214a6638a639214458203da4b67a65d14f
hana <hanadegefa344@gmail.com>
2022-12-22 06:38:57 -0800

    badbetty

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,43 +1,36 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string using rot13
- * @str:the string targeted 
- *Return: returns the encoded string
+ * rot13 - encode string using rot13
+ * @s: string to encode
+ * return: encoded string
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int index1, index2;
 
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-		                                                                                 'G', 'H', 'I', 'J', 'K', 'L',
-												 'M', 'N', 'O', 'P', 'Q', 'R',
-												 'S', 'T', 'U', 'V', 'W', 'X',
-												 'Y', 'Z', 'a', 'b', 'c', 'd',
-												 'e', 'f', 'g', 'h', 'i', 'j',
-												 'k', 'l', 'm', 'n', 'o', 'p',
-												 'q', 'r', 's', 't', 'u', 'v',
-												 'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		                                                                                 'T', 'U', 'V', 'W', 'X', 'Y',
-												 'Z', 'A', 'B', 'C', 'D', 'E',
-												 'F', 'G', 'H', 'I', 'J', 'K',
-												 'L', 'M', 'n', 'o', 'p', 'q',
-												 'r', 's', 't', 'u', 'v', 'w',
-												 'x', 'y', 'z', 'a', 'b', 'c',
-												 'd', 'e', 'f', 'g', 'h', 'i',
-												 'j', 'k', 'l', 'm',};
-	while (str[++index1])
-	{
-		for (index2 = 0; index2 < 52; index2++)
-		{
-			if (str[index1] == alphabet[index2])
-			{
-				str[index1] = rot13key[index2];
-				break;
-			}
-		}
-	}
-	return (str);
+int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+	     'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+	     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+	     'w', 'x', 'y', 'z'};
+int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+	     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+	     'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+	     'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+	     'j', 'k', 'l', 'm'};
+
+int i, j;
+
+for (j = 0; s[j] != '\0'; j++) /*loop through string*/
+{
+i = 0;
+while (a[i] != '\0' && s[j] != a[i]) /*loop through rot13 arr*/
+i++;
+
+if (s[j] == a[i]) /*if alpha matches, set to index in b arr*/
+	s[j] = b[i];
+}
+
+return (s);
 }
