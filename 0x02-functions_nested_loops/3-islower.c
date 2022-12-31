a96a1fbfe3915e112cc32414376b47104c568f3e
hana <hanadegefa344@gmail.com>
2022-12-31 03:20:28 -0800

    0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: Always return main
+ *
+ */
+int main(void)
+{
+	int n;
+	((0));
+	n = () - / 2;
+        (r + '0');
+
+	if (n > 0)
+	return 0
+
