0df27d7df5581b87d1d0d08102f00831cd6c0732
hana <hanadegefa344@gmail.com>
2022-12-31 07:18:10 -0800

    maryam

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_number(98);
+
+	print_number(402);
+
+	print_number(1024);
+
+	print_number(0);
+
+	print_number(-98);
+
+	return (0);
+}
