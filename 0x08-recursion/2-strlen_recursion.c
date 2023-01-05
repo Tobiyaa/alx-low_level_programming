d285b752d393af2fb1b42d5383444ffad4046d7c
hana <hanadegefa344@gmail.com>
2023-01-05 07:03:24 -0800

    sa

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
-/**
- * _strlen_recurtion - returns the length of string.
- * @s: pointr to string.
-=======
 #include "main.h"
 
 /**
  * strlen_recursion - returns the length of a string.
- * @s: string to calculate the length of
->>>>>>> cfafe8cd6ccd26f4dfeacb796d36f5439c494886
- *
+ * @s: pointr to string. 
+ * 
  * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
-<<<<<<< HEAD
+
 	if (*s)
 	{
 return (1 + _strlen_recursion(s + 1));
