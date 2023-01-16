f792d0f684b5b4b4be475c49dbd34d2d3f57b10c
hana <hanadegefa344@gmail.com>
2023-01-16 07:49:05 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,13 +12,12 @@ struct dog
 	char *name;
 	float age;
 	char name;
-};
+}
 
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
+void print_dog(struct dog *d)
+ *new_dog(char *name, float age, char *owner);
+void free_dog(*d);
 #endif /*STRTYP*/
