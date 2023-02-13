fa420a9aedcde3102232f45677405b47b1ddcf8c
hana <hanadegefa344@gmail.com>
2023-02-12 23:38:04 -0800

    tf

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,13 +2,8 @@
 
 /**
  * strlen_recursion - returns the length of a string.
-<<<<<<< HEAD
- * @s: pointer to string. 
- *
-=======
+ * @s: pointer to string.  
  * @s: pointr to string. 
- * 
->>>>>>> 146632dbc4f8fce11867db2112b17cfb156d5567
  * Return: length of the string
  */
 int _strlen_recursion(char *s)
@@ -24,11 +19,8 @@ return (0);
 	}
 
 }
-
-
-=======
-	if (*s == '\0')
+      if (*s == '\0')
 		return (0);
 	return (1 + _strlen_recursion(s + 1));
 }
->>>>>>> cfafe8cd6ccd26f4dfeacb796d36f5439c494886
+
